71ff15b6a7700266df6b14673464d7368dbe6d38
Chris Vensko <chrisvensko@gmail.com>
2014-05-15 12:13:54 -0700

    Moved sql out of server script.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var mysql   = require('mysql'),
     express = require('express'),
     _ = require('underscore')
-    settings = require('./settings.js');
+    settings = require('./settings.js')
+    sql = require('./sql.js');
 
 // Connection Info
 var dbConnectionInfo = settings.db;
@@ -16,12 +17,12 @@ db.connect(function(err) {
   if (err) {
     console.log(err);
   } else {
-    console.log('Connected to ' + dbConnectionInfo.host);
+    console.log('Connected to db:' + dbConnectionInfo.host);
   }
 });
 
 var clearTempTable = function() {
-  db.query('DROP TEMPORARY TABLE matching_object_ids', function() {});
+  db.query(sql.drop_temp_table(), function() {});
 };
 
 var getObjectDetails = function(err, res, req) {
@@ -33,7 +34,7 @@ var getObjectDetails = function(err, res, req) {
   var current_id = null;
   var current_object = null;
 
-  var query = db.query('SELECT i.*, object_type.string AS object_type, name.string AS field, value.string AS val FROM matching_object_ids AS i LEFT JOIN objects AS O ON i.object_id = O.object_id LEFT JOIN string_dim AS object_type ON O.type_key = object_type.string_id LEFT JOIN property_bridge AS pb ON O.object_id = pb.object_key LEFT JOIN property_dim AS pd ON pb.property_key = pd.property_id LEFT JOIN string_dim AS name ON pd.name_key = name.string_id LEFT JOIN string_dim AS value ON pd.value_key = value.string_id');
+  var query = db.query(sql.get_matching_objects());
 
   query
     .on('error', function(e) { res.send(e); })
@@ -86,7 +87,7 @@ var getObjectDetails = function(err, res, req) {
 };
 
 app.get('/', function(req, res) {
-  db.query('SELECT DISTINCT O.type_key, type.string AS object_type FROM objects AS O LEFT JOIN string_dim AS type ON O.type_key = type.string_id', function(err, rows) {
+  db.query(sql.get_object_types(), function(err, rows) {
 
     if (err) {
       console.log(err);
@@ -105,37 +106,19 @@ app.get('/', function(req, res) {
 
 app.get('/object/:id', function(req, res) {
   clearTempTable();
-  db.query('CREATE TEMPORARY TABLE matching_object_ids SELECT object_id FROM objects AS O WHERE object_id = ' + req.params.id, function(err) {
+  db.query(sql.create_temp_from_id(req.params.id), function(err) {
     getObjectDetails(err, res, req);
   });
 });
 
 app.get('/:type', function(req, res) {
   clearTempTable();
-  var where = [];
-  var sql = "CREATE TEMPORARY TABLE matching_object_ids SELECT object_id FROM objects AS O " +
-    "LEFT JOIN string_dim AS object_type ON O.type_key = object_type.string_id ";
 
-  where.push(" WHERE object_type.string = '" + req.params.type + "'");
-
-  _.each(req.query, function(val, key) {
-    var pb_table = '`pb' + key + '`';
-    var pd_table = '`pd' + key + '`';
-    var field_table = '`name' + key + '`';
-    var value_table = '`value' + key + '`';
-    sql += " LEFT JOIN property_bridge AS " + pb_table + " ON O.object_id = " + pb_table + ".object_key LEFT JOIN property_dim AS " + pd_table + " ON " + pb_table + ".property_key = " + pd_table + ".property_id LEFT JOIN string_dim AS " + field_table + " ON " + pd_table + ".name_key = " + field_table + ".string_id "
-
-    sql += " LEFT JOIN string_dim AS " + value_table + " ON " + pd_table + ".value_key = " + value_table + ".string_id";
-    where.push(field_table + ".string = '" + key + "'");
-    where.push(value_table + ".string = '" + val + "'");
-  });
-
-  sql += where.join(' AND ');
-
-  db.query(sql, function(err) {
+  var query = sql.create_temp_from_query(req.params.type, req.query);
+  db.query(query, function(err) {
     if (err) {
       console.log(err);
-      console.log(sql);
+      console.log(query);
     }
     
     getObjectDetails(err, res, req);
---
+++ sql.js
@@ -0,0 +1,64 @@
+var _ = require('underscore');
+
+exports.drop_temp_table = function() {
+  return "DROP TEMPORARY TABLE matching_object_ids";
+};
+
+exports.get_matching_objects = function() {
+  return "SELECT"
+    + " i.*"
+    + ", object_type.string AS object_type"
+    + ", name.string AS field"
+    + ", value.string AS val"
+    + " FROM matching_object_ids AS i"
+    + " LEFT JOIN objects AS O ON i.object_id = O.object_id"
+    + " LEFT JOIN string_dim AS object_type ON O.type_key = object_type.string_id"
+    + " LEFT JOIN property_bridge AS pb ON O.object_id = pb.object_key"
+    + " LEFT JOIN property_dim AS pd ON pb.property_key = pd.property_id"
+    + " LEFT JOIN string_dim AS name ON pd.name_key = name.string_id"
+    + " LEFT JOIN string_dim AS value ON pd.value_key = value.string_id";
+};
+
+exports.get_object_types = function() {
+  return "SELECT"
+    + " DISTINCT O.type_key"
+    + ", type.string AS object_type"
+    + " FROM objects AS O"
+    + " LEFT JOIN string_dim AS type ON O.type_key = type.string_id";
+};
+
+exports.create_temp_from_id = function(id) {
+  return "CREATE TEMPORARY TABLE matching_object_ids"
+    + " SELECT object_id"
+    + " FROM objects AS O"
+    + " WHERE object_id = '" + id + "'";
+};
+
+exports.create_temp_from_query = function(type, filters) {
+  var where = [];
+
+  var sql = "CREATE TEMPORARY TABLE matching_object_ids"
+    + " SELECT object_id FROM objects AS O"
+    + " LEFT JOIN string_dim AS object_type ON O.type_key = object_type.string_id";
+
+  where.push(" WHERE object_type.string = '" + type + "'");
+
+  _.each(filters, function(val, key) {
+    var pb_table = '`pb' + key + '`';
+    var pd_table = '`pd' + key + '`';
+    var field_table = '`name' + key + '`';
+    var value_table = '`value' + key + '`';
+
+    sql += " LEFT JOIN property_bridge AS " + pb_table + " ON O.object_id = " + pb_table + ".object_key"
+    + " LEFT JOIN property_dim AS " + pd_table + " ON " + pb_table + ".property_key = " + pd_table + ".property_id"
+    + " LEFT JOIN string_dim AS " + field_table + " ON " + pd_table + ".name_key = " + field_table + ".string_id"
+    + " LEFT JOIN string_dim AS " + value_table + " ON " + pd_table + ".value_key = " + value_table + ".string_id";
+
+    where.push(field_table + ".string = '" + key + "'");
+    where.push(value_table + ".string = '" + val + "'");
+  });
+
+  sql += where.join(' AND ');
+
+  return sql;
+};
